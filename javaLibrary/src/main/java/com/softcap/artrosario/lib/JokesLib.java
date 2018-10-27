e7d2cd1532f51ff2e64df6435a47e3ed9464eaad
Art Rosario <artrosario1@gmail.com>
2018-10-26 23:39:40 -0400

    corrected project
    fixed free and paid flavors

--- javaLibrary/src/main/java/com/softcap/artrosario/lib/JokesLib.java
+++ javaLibrary/src/main/java/com/softcap/artrosario/lib/JokesLib.java
@@ -1,8 +1,17 @@
 package com.softcap.artrosario.lib;
 
-public class JokesLib {
+import java.util.Random;
 
+public class JokesLib {
+    public String[] jokesArray = {"How do functions break up? \"They stop calling each other!\"",
+            "In what unit do you measure a function's length? \"In Para meters...\"",
+            "What did the flirtatious function say? \"Call me ;)\"",
+            "Q: How do you tell an introverted computer scientist from an extroverted computer scientist?\n" +
+                    "\n" +
+                    "A: An extroverted computer scientist looks at your shoes when he talks to you."
+    };
     public String tellAHandCraftedJoke(){
+           // return jokesArray[new Random().nextInt(jokesArray.length)];
             return "A hand-crafted joke";
         }
 
